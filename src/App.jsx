1e59d6f7937044c38acd71b7605eaf1272cb1eb3
ekimov-fs <ekimsergevgen@yandex.ru>
2026-01-23 21:22:11 +0300

    feah: Added CSS styles and an About Me block

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,24 @@ function App() {
         <h2 style={{color:'#4824ff', fontSize: '40px' }}>Контакты</h2>
         <p style={{fontSize: '22px'}}>Вы можете связаться со мной в Телеграм <br/> или Инстаграм</p>
       </ModalWindow>
+
+      <div className="welcom-block">
+        <div className="first-block">
+          <h1>Веб разработчик <span className='title'>EkimovS</span></h1>
+          <h2 style={{marginBottom: '7%', marginTop: '7%'}}>Создаю уникальные <span style={{color: '#4824ff'}}>Web</span> продукты <br />
+             под ваши задачи. 
+          </h2>
+          <h3>Занимаюсь разработкой <br />
+          на протяжении <span style={{color: '#4824ff'}}>2 лет</span>.
+          </h3>
+        </div>
+        <div className="image-block" style={{width:'100%', display: 'flex',
+          justifyContent: 'center', alignItems: 'center'
+        }}>
+          <h1>Здесь будет анимированное изображение </h1>
+        </div>
+        
+      </div>
     </>
   );
 };
